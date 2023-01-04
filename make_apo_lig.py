4e07dc38bb7d32c60b858d91530edcaf62514235
m92rizk <93646870+m92rizk@users.noreply.github.com>
2023-01-04 11:54:55 +0100

    Create make_apo_lig.py

---
+++ make_apo_lig.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+#SBATCH -p nice
+#SBATCH -n 10
+#SBATCH -t 7:0:0
+#SBATCH -J rizk_stats
+
+import os, sys, re, shutil
+import numpy as np
+import copy
+from optparse import OptionParser
+
+dir = os.getcwd()
+
+parser = OptionParser()
+
+parser.add_option("-l", "--ligand", dest="lig")
+parser.add_option("-c", "--chain", dest="ch", default="A")
+parser.add_option("-p", "--model", dest="pdb")
+parser.add_option("", "--apo", action="store_true", dest="apo", default=False)
+parser.add_option("", "--compound", action="store_true", dest="compound", default=False)
+
+
+
+(options, args) = parser.parse_args()
+
+if options.lig is None:
+    lig = raw_input ("Enter ligand (or amino acid) name: ")
+else:
+    lig = options.lig
+
+LIG = lig.upper()
+apo = options.apo
+compound = options.compound
+
+if options.pdb is None:
+    pdb = raw_input ("Enter pdb name: ")
+else:
+    pdb = options.pdb
+
+if os.path.isfile('./apo_'+str(pdb)+'.pdb') and apo is True :
+   os.remove('./apo_'+str(pdb)+'.pdb')
+
+if os.path.isfile(str(LIG)+'.pdb') and compound is True :
+   os.remove(str(LIG)+'.pdb')  
+    
+file = open(str(pdb)+'.pdb', "r")
+length = 0
+for line in file:
+    if (re.search(str(LIG), line) and re.search("HETATM", line)) or (re.search(str(LIG), line) and re.search("ATOM", line)):
+        val = str(line[21:22]) # val is the chain
+        if str(val) != str(options.ch) and apo is True:
+           with open('apo_'+str(pdb)+'.pdb', "a") as outfile:
+               outfile.write(str(line))
+        elif str(val) == str(options.ch) and compound is True:
+            with open(str(LIG)+'.pdb', "a") as outfile:
+               outfile.write(str(line))
+    else:
+        if apo is True:
+            with open('apo_'+str(pdb)+'.pdb', "a") as outfile:
+                outfile.write(str(line))
+    if not (re.search("HETATM", line) or re.search("ATOM", line)):
+        if compound is True:
+            with open(str(LIG)+'.pdb', "a") as outfile:
+               outfile.write(str(line))
